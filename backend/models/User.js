62d053ca56219c48e7bc0d8abbc3e0d1ed44b364
Taron Bentley <taronbentley1@gmail.com>
2026-05-29 14:50:50 -0400

    add user model with schema, password hashing, and email validation

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema, "villains");
+
+module.exports = User;
