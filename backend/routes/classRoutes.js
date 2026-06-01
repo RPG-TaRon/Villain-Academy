221d5d6107b2b366f0d19365e1440399e64e1c8a
Taron Bentley <taronbentley1@gmail.com>
2026-06-01 13:56:11 -0400

    fix route order for class retrieval and creation in classRoutes

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -3,11 +3,13 @@ const router = require("express").Router();
 const {
   createClass,
   getClasses,
+  getClassById,
 } = require("../controllers/classController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", authMiddleware, createClass);
 router.get("/", authMiddleware, getClasses);
+router.get("/:id", authMiddleware, getClassById);
+router.post("/", authMiddleware, createClass);
 
 module.exports = router;
