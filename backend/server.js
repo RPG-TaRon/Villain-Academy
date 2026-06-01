2e13f7e0b33f661eb1b198aecf8c077a80dff422
Taron Bentley <taronbentley1@gmail.com>
2026-06-01 08:45:35 -0400

    refactor server setup to include route imports and default port configuration

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/connection");
+const userRoutes = require("./routes/userRoutes");
+const classRoutes = require("./routes/classRoutes");
+const assignmentRoutes = require("./routes/assignmentRoutes");
+
 const app = express();
+
 connectDB();
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 1313;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Villain Academy API Running");
 });
+
+app.use("/api/users", userRoutes);
+app.use("/api/classes", classRoutes);
+app.use("/api/assignments", assignmentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
