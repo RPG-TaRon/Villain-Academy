2dcd5b918ed249d77ff52eef4a7375d7afa0d166
Taron Bentley <taronbentley1@gmail.com>
2026-06-01 08:58:36 -0400

    add user routes for registration, login, and profile retrieval

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
+
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/me", authMiddleware, getMe);
+
+module.exports = router;
