21de0f1dc6442cb530d72b85e13ac017d25abede
Taron Bentley <taronbentley1@gmail.com>
2026-06-01 11:08:27 -0400

    refactor userSchema pre-save hook to remove unnecessary next() call

--- backend/models/User.js
+++ backend/models/User.js
@@ -30,12 +30,10 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (this.isNew || this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
-
-  next();
 });
 
 userSchema.methods.isCorrectPassword = async function (password) {
