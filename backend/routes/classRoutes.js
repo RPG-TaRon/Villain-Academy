9887fb3c252dd5f18a8e573960252245c6968fea
Taron Bentley <taronbentley1@gmail.com>
2026-06-02 12:13:34 -0400

    add DELETE route for class deletion by authenticated instructors

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -5,6 +5,7 @@ const {
   getClasses,
   getClassById,
   updateClass,
+  deleteClass,
 } = require("../controllers/classController");
 
 const authMiddleware = require("../middleware/authMiddleware");
@@ -13,5 +14,6 @@ router.get("/", authMiddleware, getClasses);
 router.get("/:id", authMiddleware, getClassById);
 router.post("/", authMiddleware, createClass);
 router.put("/:id", authMiddleware, updateClass);
+router.delete("/:id", authMiddleware, deleteClass);
 
 module.exports = router;
