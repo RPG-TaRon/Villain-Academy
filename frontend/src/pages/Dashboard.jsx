1c4511fdfb18b8015f691791716dc1b7a1c8d079
Taron Bentley <taronbentley1@gmail.com>
2026-06-02 18:55:17 -0400

    implement Dashboard component with user greeting and class management

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,23 @@
+import { useAuth } from "../context/AuthContext";
+
 function Dashboard() {
-  return <h1>Academy Dashboard</h1>;
+  const { user } = useAuth();
+
+  return (
+    <main>
+      <h1>Villain Dashboard</h1>
+
+      <h2>Welcome, {user?.username}</h2>
+
+      <button>Create New Class</button>
+
+      <div>
+        <h3>Here's your Classes Lackie</h3>
+
+        <p>You don't have classes you fake Villain.</p>
+      </div>
+    </main>
+  );
 }
 
 export default Dashboard;
