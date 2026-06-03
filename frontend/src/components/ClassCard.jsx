e9a98d06732db9f5e452150bbb5e67f12a197f66
Taron Bentley <taronbentley1@gmail.com>
2026-06-03 15:14:09 -0400

    add ClassCard component: implement class editing and display functionality

---
+++ frontend/src/components/ClassCard.jsx
@@ -0,0 +1,60 @@
+import { Link } from "react-router-dom";
+
+function ClassCard({
+  classItem,
+  editingClassId,
+  editFormData,
+  handleEditChange,
+  startEditing,
+  updateClass,
+  cancelEditing,
+  deleteClass,
+}) {
+  return (
+    <div>
+      {editingClassId === classItem._id ? (
+        <>
+          <input
+            type="text"
+            name="name"
+            value={editFormData.name}
+            onChange={handleEditChange}
+          />
+
+          <input
+            type="text"
+            name="description"
+            value={editFormData.description}
+            onChange={handleEditChange}
+          />
+
+          <button onClick={() => updateClass(classItem._id)}>
+            Save Evil Changes
+          </button>
+
+          <button onClick={cancelEditing}>
+            Cancel This Nonsense
+          </button>
+        </>
+      ) : (
+        <>
+          <Link to={`/classes/${classItem._id}`}>
+            <h4>{classItem.name}</h4>
+          </Link>
+
+          <p>{classItem.description}</p>
+
+          <button onClick={() => startEditing(classItem)}>
+            Edit This Scheme
+          </button>
+
+          <button onClick={() => deleteClass(classItem._id)}>
+            Expel This Class From The Academy
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ClassCard;
