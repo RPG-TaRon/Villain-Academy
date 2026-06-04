7af43d18a34d24c097e1903eaeb0c39e1e4f04d8
Taron Bentley <taronbentley1@gmail.com>
2026-06-04 00:04:22 -0400

    feat: refactor admin routes to improve structure and readability

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,16 +2,35 @@ const router = require("express").Router();
 
 const {
   getAcademyData,
+  deleteUserByAdmin,
+  deleteClassByAdmin,
+  deleteAssignmentByAdmin,
 } = require("../controllers/adminController");
 
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
-router.get(
-  "/academy",
+router.get("/academy", authMiddleware, adminMiddleware, getAcademyData);
+
+router.delete(
+  "/users/:userId",
   authMiddleware,
   adminMiddleware,
-  getAcademyData
+  deleteUserByAdmin
+);
+
+router.delete(
+  "/classes/:classId",
+  authMiddleware,
+  adminMiddleware,
+  deleteClassByAdmin
+);
+
+router.delete(
+  "/assignments/:assignmentId",
+  authMiddleware,
+  adminMiddleware,
+  deleteAssignmentByAdmin
 );
 
 module.exports = router;
